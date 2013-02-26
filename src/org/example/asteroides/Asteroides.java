2543ee2ea13001d61d6ed54288058b427b7e73b1
Carlos Bautista <carlosbm@gmail.com>
2013-02-27 00:04:36 +0100

    Added AcercaDe option and message

---
+++ src/org/example/asteroides/Asteroides.java
@@ -0,0 +1,47 @@
+package org.example.asteroides;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Asteroides extends Activity {
+
+	public void lanzarAcercaDe(View view) {
+
+		Intent i = new Intent(this, AcercaDe.class);
+
+		startActivity(i);
+
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+		/** true -> el menú ya está visible */
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.acercaDe:
+			// lanzarAcercaDe(null);
+			break;
+		}
+		return true;
+		/** true -> consumimos el item, no se propaga */
+	}
+
+}
